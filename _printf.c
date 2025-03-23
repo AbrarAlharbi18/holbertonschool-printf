e793bbd2b43e7ade7b6f0d3675f04f0224dd497c
AbrarAlharbi18 <Abrarsaadalharbi@gmail.com>
2025-03-23 17:23:10 +0000

    task0

--- _printf.c
+++ _printf.c
@@ -1,61 +1,46 @@
 #include "main.h"
-
 /**
- * _printf - Custom printf function
- * @format: Format string
- * Return: Number of characters printed
+ * _printf - is a function that selects the correct function to print.
+ * @format: identifier to look for.
+ * Return: the length of the string.
  */
-int _printf(const char *format, ...)
+int _printf(const char * const format, ...)
 {
-	va_list args;
-	int count = 0;
+	convert p[] = {
+		{"%s", print_s}, {"%c", print_c},
+		{"%%", print_37},
+		{"%i", print_i}, {"%d", print_d}, {"%r", print_revs},
+		{"%R", print_rot13}, {"%b", print_bin},
+		{"%u", print_unsigned},
+		{"%o", print_oct}, {"%x", print_hex}, {"%X", print_HEX},
+		{"%S", print_exc_string}, {"%p", print_pointer}
+	};
 
-	if (!format)
-		return (-1);
+	va_list args;
+	int i = 0, j, length = 0;
 
 	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
 
-	while (*format)
+Here:
+	while (format[i] != '\0')
 	{
-		if (*format == '%')
+		j = 13;
+		while (j >= 0)
 		{
-			format++;
-			if (*format == '\0')  /* Handle case where '%' is the last character */
+			if (p[j].ph[0] == format[i] && p[j].ph[1] == format[i + 1])
 			{
-				count += _putchar('%');
-				break;
+				length += p[j].function(args);
+				i = i + 2;
+				goto Here;
 			}
-			count += handle_format_specifier(*format, args);
+			j--;
 		}
-		else
-		{
-			count += _putchar(*format);
-		}
-		format++;
+		_putchar(format[i]);
+		length++;
+		i++;
 	}
-
 	va_end(args);
-	return (count);
-}
-
-/**
- * handle_format_specifier - Handles format specifiers
- * @specifier: Format specifier character
- * @args: va_list of arguments
- * Return: Number of characters printed
- */
-int handle_format_specifier(char specifier, va_list args)
-{
-	switch (specifier)
-	{
-		case 'c':
-			return (print_char(va_arg(args, int)));
-		case 's':
-			return (print_string(va_arg(args, char *)));
-		case '%':
-			return (print_percent());
-		default:
-			/* If the specifier is invalid, print '%' followed */
-			return (_putchar('%') + _putchar(specifier));
-	}
+	return (length);
 }
