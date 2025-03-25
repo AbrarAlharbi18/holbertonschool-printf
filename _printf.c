789d91a82f91d57c02901c32bfa19ff86395344b
Mohammed0K <M.H.D-6@hotmail.com>
2025-03-25 03:47:34 +0000

    Mohammed try

--- _printf.c
+++ _printf.c
@@ -1,61 +1,19 @@
 #include "main.h"
-
 /**
- * _printf - Custom printf function
- * @format: Format string
- * Return: Number of characters printed
+ * _printf - Produces output according to a format.
+ * @format: The format string.
+ *
+ * Return: Total number of characters printed.
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int count = 0;
+	int count;
 
 	if (!format)
 		return (-1);
-
 	va_start(args, format);
-
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == '\0')  /* Handle case where '%' is the last character */
-			{
-				count += _putchar('%');
-				break;
-			}
-			count += handle_format_specifier(*format, args);
-		}
-		else
-		{
-			count += _putchar(*format);
-		}
-		format++;
-	}
-
+	count = process_format(format, args);
 	va_end(args);
 	return (count);
-}
-
-/**
- * handle_format_specifier - Handles format specifiers
- * @specifier: Format specifier character
- * @args: va_list of arguments
- * Return: Number of characters printed
- */
-int handle_format_specifier(char specifier, va_list args)
-{
-	switch (specifier)
-	{
-		case 'c':
-			return (print_char(va_arg(args, int)));
-		case 's':
-			return (print_string(va_arg(args, char *)));
-		case '%':
-			return (print_percent());
-		default:
-			/* If the specifier is invalid, print '%' followed */
-			return (_putchar('%') + _putchar(specifier));
-	}
 }
--- main.h
+++ main.h
@@ -7,10 +7,8 @@
 
 /* Function Prototypes */
 int _printf(const char *format, ...);
-int handle_format_specifier(char specifier, va_list args);
-int _putchar(char c);
-int print_char(char c);
-int print_string(char *str);
-int print_percent(void);
-
+int process_format(const char *format, va_list args);
+int handle_conversion(char specifier, va_list args);
+int print_number(int n);
+int print_unsigned_number(unsigned int n);
 #endif /* MAIN_H */
