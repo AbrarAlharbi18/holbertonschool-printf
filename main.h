8d19b369f8eb88f12e838785e5add5a5f045fa22
Mohammed0K <M.H.D-6@hotmail.com>
2025-03-25 03:51:03 +0000

    Mohammed try

--- main.h
+++ main.h
@@ -5,8 +5,13 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-/* Function Prototypes */
+/* Function Prototype */
 int _printf(const char *format, ...);
+int handle_format_specifier(char specifier, va_list args);
+int _putchar(char c);
+int print_char(char c);
+int print_string(char *str);
+int print_percent(void);
 int process_format(const char *format, va_list args);
 int handle_conversion(char specifier, va_list args);
 int print_number(int n);
